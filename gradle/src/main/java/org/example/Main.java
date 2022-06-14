691fd51127c0d5e109730ef3099b9ac8596f5d4c
Victor <Victor@LAPTOP-4918OAIF>
2022-06-14 21:42:41 +0200

    Dependencies aktualisiert für Spark und Testkommunikation mit Client in Main

--- gradle/src/main/java/org/example/Main.java
+++ gradle/src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import static spark.Spark.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        post("/games/hangman/start", (q, a) -> "{ Herzlich Willkommen vom Server! }");
+
     }
 }
