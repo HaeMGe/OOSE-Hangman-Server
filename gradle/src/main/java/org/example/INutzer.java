ed49fd310a388d87868b0d08073e31eb7611ebe6
Victor <Victor@LAPTOP-4918OAIF>
2022-06-17 21:33:03 +0200

    Interface für Nutzer-Klasse und Javadoc

---
+++ gradle/src/main/java/org/example/INutzer.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import static org.example.Main.warteraum;
+
+public interface INutzer {
+    /**
+     * Gibt Nutzername zurück
+     * @return Name als String
+     */
+    public String getName();
+    /**
+     * Gibt Nutzerpasswort zurück
+     * @return Passwort als String
+     */
+    public String getPasswort();
+    /**
+     * Gibt Punktescore zurück
+     * @return erspielte Punkte insgesamt
+     */
+    public int getPunktestand();
+
+    /**
+     * Neuen Punktestand aktualisieren
+     * @param neuerWert neuer Punktestand
+     */
+    public void setPunktestand(int neuerWert);
+
+
+    /**
+     * Ein Spiel beginnen, wenn Gegner gefunden
+     * @return String, der für die Client-Server-Kommunikation wichtig ist
+     */
+    public String start();
+
+    /**
+     * Interne Implementierung von start()
+     * @param counter Zähler, wie lange auf Mitspieler gewartet werden soll
+     * @return Meldung, ob Spielpartner gefunden wurde
+     */
+    private String start(int counter){return null;}
+
+
+    /**
+     * Nutzer mit seinem Namen und Punkten als String ausgeben
+     * @return Nutzername und Punkte
+     */
+    public String toString();
+}
+
