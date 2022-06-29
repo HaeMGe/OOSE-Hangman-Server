7f182849266f0551cdbcba1b97141a40119d4f59
Tuhin <75586604+HaeMGe@users.noreply.github.com>
2022-06-29 21:45:28 +0200

    Liste mit 1000 oder so meistbenutzten Nomen der deutschen Sprache hinzugefügt

--- gradle/src/main/java/org/example/Main.java
+++ gradle/src/main/java/org/example/Main.java
@@ -20,7 +20,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-
+        //1000 wörter oder so aus der .txt datei in liste tun
         woerter = Files.readAllLines(Paths.get("C:\\Users\\Tuhin Thodeme\\IdeaProjects\\hangmanserver\\gradle\\src\\main\\java\\org\\example\\woerter.txt"));
 
         //System.out.println(content);
