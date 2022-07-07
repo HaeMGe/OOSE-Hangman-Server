6121baeec2f9229f66b84565df6a621921430889
Victor <Victor@LAPTOP-4918OAIF>
2022-07-07 13:23:40 +0200

    JavaDoc für PoolKlasse

--- gradle/src/main/java/org/example/Pool.java
+++ gradle/src/main/java/org/example/Pool.java
@@ -1,7 +1,9 @@
 package org.example;
 
-import java.util.ArrayList;
-
+/**
+ * Jedes Spiel wird in der Umgebung eines Pools durchgeführt, welcher hier implementiert ist. Ein Pool hält die Informationen zu dem gespielten
+ * Spiel, dem gewählten Schwierigkeitsgrad und der Pool-ID.
+ */
 public class Pool {
     Game spiel; //zugehoeriges Spiel
     //ArrayList<Nutzer> mitglieder = new ArrayList<>();  //1 oder 2 Spieler
@@ -16,16 +18,22 @@ public class Pool {
        this.spiel = new Game(level);  //Neues Spiel mit richtigem Schwierigkeitsgrad erschaffen
         Main.poolListe.add(this); //Zur Poolliste hinzufügen
         initiator.leben = spiel.leben;
-        spiel.members.add(initiator);
+        spiel.members.add(initiator);   //Initiator ist sofort Mitglied im Pool
     }
 
-
+    /**
+     * Immer 1 oder 2, bei 2 kann ein Spiel gestartet werden.
+     * @return Anzahl der Spieler
+     */
     public int anzahlSpieler(){
         return spiel.members.size();
     }
 
 
-
+    /**
+     * Liefert den Pool als String mit Informationen über seine ID, Schwierigkeitsgrad und Mitgliederanzahl.
+     * @return Poolinfos als String
+     */
     public String toString(){
         return "ID: " + id + " Nummer in Liste: " + Main.poolListe.indexOf(this) + ", Schwierigkeit: " + level + " Mitgliederzahl: " + anzahlSpieler();
     }
